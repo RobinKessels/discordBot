521d6be12ec9520b1bc03e654bf4c2bd47cf6189
RobinKessels <44303721+RobinKessels@users.noreply.github.com>
2019-02-08 14:25:07 +0100

    Add files via upload

---
+++ robbot.py
@@ -0,0 +1,56 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event  # event decorator/wrapper
+async def on_ready():  # method expected by client. This runs once when connected
+    print(f'We have logged in as {client.user}')  # notification of login.
+
+
+@client.event
+async def on_message(message):  # event that happens per any message.
+    # each message has a bunch of attributes. Here are a few.
+    # check out more by print(dir(message)) for example.
+    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
+    server = client.get_guild() # paste your server ID here
+
+    if "hey robbot" in message.content.lower():
+        if str(message.author) == "Skorter#0896":
+            await message.channel.send("HELLO MAKER!")
+        else:
+            await message.channel.send("HELLO " + str(message.author.name).upper() + "!")
+
+    elif "bye robbot" == message.content.lower() and str(message.author) == "Skorter#0896":
+        await message.channel.send("I'LL BE BACK!")
+        await client.close()
+        exit()
+
+    elif "!membercount" == message.content.lower():
+        await message.channel.send(f"```{server.member_count}```")
+
+    elif "!members" == message.content.lower():
+        online = 0
+        idle = 0
+        offline = 0
+
+        for m in server.members:
+            if str(m.status) == "online":
+                online += 1
+            if str(m.status) == "offline":
+                offline += 1
+            else:
+                idle += 1
+
+        await message.channel.send(f"```Online: {online}\nIdle/busy/dnd: {idle}\nOffline: {offline}```")
+
+    elif "!bo archers" == message.content.lower():
+        file = discord.File(r"C:\Users\Robin\Pictures\aoebo\archers.png", filename="archers.png")
+        await message.channel.send("archers.png", file=file)
+
+    elif "!bo fcuu" == message.content.lower() or "!bo fc uu" == message.content.lower():
+        file = discord.File(r"C:\Users\Robin\Pictures\aoebo\fcuu.png", filename="fcuu.png")
+        await message.channel.send("fcuu.png", file=file)
+
+client.run("NTQzMzg4Nzc3MjU5ODU5OTY4.Dz73rA.SDE_tk3rmDeCDDbYn0nYit07UPM")
